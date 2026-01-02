c088aa4707e83f2f6303f164b72a80d87c13fd1d
helangeorge999 <helangeorge78@gmail.com>
2026-01-02 11:44:49 +0545

    update code and pubspec yaml

--- lib/features/auth/presentation/pages/login_page.dart
+++ lib/features/auth/presentation/pages/login_page.dart
@@ -1,1 +1,62 @@
+import 'package:flutter/material.dart';
+import '../../../../core/widgets/app_button.dart';
+import '../../../../core/widgets/app_text_field.dart';
 
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final isTablet = MediaQuery.of(context).size.width > 600;
+
+    return Scaffold(
+      body: Center(
+        child: SizedBox(
+          width: isTablet ? 420 : double.infinity,
+          child: Padding(
+            padding: const EdgeInsets.all(24),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  'Log In',
+                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 24),
+
+                const AppTextField(
+                  hint: 'Email or Phone Number',
+                  icon: Icons.email,
+                ),
+                const SizedBox(height: 16),
+
+                const AppTextField(
+                  hint: 'Password',
+                  icon: Icons.lock,
+                  obscure: true,
+                ),
+                const SizedBox(height: 24),
+
+                AppButton(title: 'Login', onPressed: () {}),
+
+                const SizedBox(height: 12),
+
+                TextButton(
+                  onPressed: () {},
+                  child: const Text('Forgot password?'),
+                ),
+
+                const SizedBox(height: 16),
+
+                OutlinedButton(
+                  onPressed: () {},
+                  child: const Text('Create account'),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/features/auth/presentation/pages/signup_page.dart
+++ lib/features/auth/presentation/pages/signup_page.dart
@@ -1,1 +1,71 @@
+import 'package:flutter/material.dart';
+import '../../../../core/widgets/app_button.dart';
+import '../../../../core/widgets/app_text_field.dart';
 
+class SignupPage extends StatelessWidget {
+  const SignupPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final isTablet = MediaQuery.of(context).size.width > 600;
+
+    return Scaffold(
+      body: Center(
+        child: SizedBox(
+          width: isTablet ? 450 : double.infinity,
+          child: Padding(
+            padding: const EdgeInsets.all(24),
+            child: ListView(
+              children: [
+                const Text(
+                  'Create Your Account',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 24),
+
+                const AppTextField(
+                  hint: 'Enter Your Full Name',
+                  icon: Icons.person,
+                ),
+                const SizedBox(height: 12),
+
+                const AppTextField(
+                  hint: 'DOB (YYYY-MM-DD)',
+                  icon: Icons.calendar_month,
+                ),
+                const SizedBox(height: 12),
+
+                const AppTextField(hint: 'Gender', icon: Icons.people),
+                const SizedBox(height: 12),
+
+                const AppTextField(hint: 'Phone Number', icon: Icons.phone),
+                const SizedBox(height: 12),
+
+                const AppTextField(
+                  hint: 'Enter Your Password',
+                  icon: Icons.lock,
+                  obscure: true,
+                ),
+                const SizedBox(height: 12),
+
+                const AppTextField(
+                  hint: 'Confirm Your Password',
+                  icon: Icons.lock_outline,
+                  obscure: true,
+                ),
+                const SizedBox(height: 24),
+
+                AppButton(title: 'Sign Up', onPressed: () {}),
+
+                const SizedBox(height: 16),
+
+                OutlinedButton(onPressed: () {}, child: const Text('Sign In')),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
