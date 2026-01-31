97ae269d0ecf19874253855816df9382809dee66
helangeorge999 <helangeorge78@gmail.com>
2026-01-31 20:58:50 +0545

    widget test code commit

--- test/unit_test/widget_test/avatar_widget_test.dart
+++ test/unit_test/widget_test/avatar_widget_test.dart
@@ -1,1 +1,11 @@
+import 'package:busbooking/features/auth/presentation/pages/profile_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
 
+void main() {
+  testWidgets('Profile avatar is displayed', (tester) async {
+    await tester.pumpWidget(const MaterialApp(home: ProfilePage()));
+
+    expect(find.byType(CircleAvatar), findsOneWidget);
+  });
+}
--- test/unit_test/widget_test/profile_page_test.dart
+++ test/unit_test/widget_test/profile_page_test.dart
@@ -1,1 +1,11 @@
+import 'package:busbooking/features/auth/presentation/pages/profile_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
 
+void main() {
+  testWidgets('Profile page loads correctly', (tester) async {
+    await tester.pumpWidget(const MaterialApp(home: ProfilePage()));
+
+    expect(find.text('Profile'), findsOneWidget);
+  });
+}
